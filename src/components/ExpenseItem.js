bbc20893599641b4a5e820fd0babed82922b1c95
unknown <olivertrain1996@gmail.com>
2022-10-04 22:03:04 +0100

    Add css file from walkthrough

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return (
-      <div>
-        <div>
-          <h2>MAYBE THIS WORKS Ayyy??</h2>
-        </div>
+  return (
+    <div className='expense-item'>
+      <div>Date</div>
+      <div className='expense-item__description'>
+        <h2>Title</h2>
+        <div className='expense-item__price'>Amount</div>
       </div>
-    );
-  }
-  
-  export default ExpenseItem;
-  
+    </div>
+  );
+}
+
+export default ExpenseItem;
