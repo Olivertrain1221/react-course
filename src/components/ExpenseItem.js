43ede29d48d53b68c69c2f5fb933822243abdc43
unknown <olivertrain1996@gmail.com>
2022-10-04 17:40:26 +0100

    Add new element

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+      <div>
+        <div>
+          <h2>MAYBE THIS WORKS Ayyy??</h2>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;
+  
